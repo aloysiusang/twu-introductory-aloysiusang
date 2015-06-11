1935915e71d062dee3947a12d38c3a5a14cd7491
Aloysius Ang <aloysiusang1990@gmail.com>
2015-06-11 17:43:47 +0800

    Initial commit

---
+++ src/com/twu/introductory/Source.java
@@ -0,0 +1,10 @@
+package com.twu.introductory;
+
+/**
+ * Created by aloysiusang on 11/6/15.
+ */
+public class Source {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ test/com/twu/introductory/SourceTest.java
@@ -0,0 +1,15 @@
+package com.twu.introductory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aloysiusang on 11/6/15.
+ */
+public class SourceTest {
+    @Test
+    public void testExample() throws Exception {
+        assertEquals(1,1);
+    }
+}
